67b111ad18c8021bcc9fb6f7836c857cfe369cfa
innogamed <inno.game.au@gmail.com>
2019-06-20 05:18:39 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-1_MXwEeZzzs', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4q8b70kusjj0s1mvlrmntpjfub', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
